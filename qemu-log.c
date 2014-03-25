8ad63149cd5dffd28262f652a26391ed350efbc9
Alex Bennée <alex.bennee@linaro.org>
2014-03-25 10:50:51 +0000

    qemu-log: correct help text for -d cpu
    This doesn't just dump CPU state on translation but on every block
    entrance.
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Reviewed-by: Andreas Färber <afaerber@suse.de>
    
    ---
    v4:
     - add r-b-tag

--- qemu-log.c
+++ qemu-log.c
@@ -105,7 +105,7 @@ const QEMULogItem qemu_log_items[] = {
     { CPU_LOG_EXEC, "exec",
       "show trace before each executed TB (lots of logs)" },
     { CPU_LOG_TB_CPU, "cpu",
-      "show CPU state before block translation" },
+      "show CPU registers before each executed TB (lots of logs)" },
     { CPU_LOG_MMU, "mmu",
       "log MMU-related activities" },
     { CPU_LOG_PCALL, "pcall",
