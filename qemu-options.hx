ac838b351dedc47a0fa6d2f9cdfe20eb119ed03b
Alex Bennée <alex.bennee@linaro.org>
2014-03-26 11:38:50 +0000

    qemu-log: new option -dfilter to limit output
    When debugging big programs or system emulation sometimes you want both
    the verbosity of cpu,exec et all but don't want to generate lots of logs
    for unneeded stuff. This patch adds a new option -dfilter which allows
    you to specify interesting address ranges in the form:
    
      -dfilter 0x8000-0x9000,0xffffffc000080000+0x200,...
    
    Then logging code can use the new qemu_log_in_addr_range() function to
    decide if it will output logging information for the given range.
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    
    ----
    
    v2
      - More clean-ups to the documentation
    
    v3
      - re-base
      - use GArray instead of GList to avoid cache bouncing

--- qemu-options.hx
+++ qemu-options.hx
@@ -2754,6 +2754,22 @@ STEXI
 Output log in @var{logfile} instead of to stderr
 ETEXI
 
+DEF("dfilter", HAS_ARG, QEMU_OPTION_DFILTER, \
+    "-dfilter range,..  filter debug output to range of addresses (useful for -d cpu,exec,etc..)\n",
+    QEMU_ARCH_ALL)
+STEXI
+@item -dfilter @var{range1}[,...]
+@findex -dfilter
+Filter debug output to that relevant to a range of target addresses. The filter
+spec can be either @var{start}-@var{end} or @var{start}+@var{size} where @var{start}
+@var{end} and @var{size} are the addresses and sizes required. For example:
+@example
+    -dfilter 0x8000-0x9000,0xffffffc000080000+0x200
+@end example
+Will dump output for any code in the 0x1000 sized block starting at 0x8000 and
+the 0x200 sized block starting at 0xffffffc000080000.
+ETEXI
+
 DEF("L", HAS_ARG, QEMU_OPTION_L, \
     "-L path         set the directory for the BIOS, VGA BIOS and keymaps\n",
     QEMU_ARCH_ALL)
