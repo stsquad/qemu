ee739a1ce50c3fa1a3e581c28f2aa8c4bf6855a0
Alex Bennée <alex.bennee@linaro.org>
2014-09-11 17:01:25 +0100

    target-arm: kvm - support for single step
    This adds support for single-step. There isn't much to do on the QEMU
    side as after we set-up the request for single step via the debug ioctl
    it is all handled within the kernel.
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    
    ---
    v2
      - convert to using HSR_EC
    v3
      - use internals.h definitions

--- target-arm/kvm.c
+++ target-arm/kvm.c
@@ -535,6 +535,13 @@ static int kvm_handle_debug(CPUState *cs, struct kvm_run *run)
     kvm_cpu_synchronize_state(cs);
 
     switch (hsr_ec) {
+    case EC_SOFTWARESTEP:
+        if (cs->singlestep_enabled) {
+            return true;
+        } else {
+            error_report("Came out of SINGLE STEP when not enabled");
+        }
+        break;
     case EC_AA64_BKPT:
         if (kvm_find_sw_breakpoint(cs, env->pc)) {
             return true;
@@ -595,6 +602,9 @@ int kvm_arch_on_sigbus(int code, void *addr)
 
 void kvm_arch_update_guest_debug(CPUState *cs, struct kvm_guest_debug *dbg)
 {
+    if (cs->singlestep_enabled) {
+        dbg->control |= KVM_GUESTDBG_ENABLE | KVM_GUESTDBG_SINGLESTEP;
+    }
     if (kvm_sw_breakpoints_active(cs)) {
         dbg->control |= KVM_GUESTDBG_ENABLE | KVM_GUESTDBG_USE_SW_BP;
     }
