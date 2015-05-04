5064309e72f01e0d96f6167a5a45b7d7874236b5
Shannon Zhao <shannon.zhao@linaro.org>
2015-05-04 15:31:24 +0800

    hw/arm/virt-acpi-build: Basic framework for building ACPI tables on ARM
    Introduce a preliminary framework in virt-acpi-build.c with the main
    ACPI build functions. It exposes the generated ACPI contents to
    guest over fw_cfg.
    
    The required ACPI v5.1 tables for ARM are:
    - RSDP: Initial table that points to XSDT
    - RSDT: Points to FADT GTDT MADT tables
    - FADT: Generic information about the machine
    - GTDT: Generic timer description table
    - MADT: Multiple APIC description table
    - DSDT: Holds all information about system devices/peripherals, pointed by FADT
    
    Signed-off-by: Shannon Zhao <zhaoshenglong@huawei.com>
    Signed-off-by: Shannon Zhao <shannon.zhao@linaro.org>

---
+++ include/hw/arm/virt-acpi-build.h
@@ -0,0 +1,70 @@
+/*
+ *
+ * Copyright (c) 2015 HUAWEI TECHNOLOGIES CO.,LTD.
+ *
+ * Author: Shannon Zhao <zhaoshenglong@huawei.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 or later, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef QEMU_VIRT_ACPI_BUILD_H
+#define QEMU_VIRT_ACPI_BUILD_H
+
+#include "qemu-common.h"
+
+typedef struct MemMap {
+    hwaddr addr;
+    hwaddr size;
+} MemMap;
+
+typedef struct AcpiGtdtInfo {
+    uint32_t timer_virt;
+    uint32_t timer_s_el1;
+    uint32_t timer_ns_el1;
+    uint32_t timer_ns_el2;
+} AcpiGtdtInfo;
+
+typedef struct AcpiMadtInfo {
+    const MemMap *gic_cpu_memmap;
+    const MemMap *gic_dist_memmap;
+} AcpiMadtInfo;
+
+typedef struct AcpiDsdtInfo {
+    const MemMap *uart_memmap;
+    const int *uart_irq;
+    const MemMap *virtio_mmio_memmap;
+    const int *virtio_mmio_irq;
+    int virtio_mmio_num;
+    const MemMap *rtc_memmap;
+    const int *rtc_irq;
+    const MemMap *flash_memmap;
+} AcpiDsdtInfo;
+
+typedef struct VirtGuestInfo {
+    int smp_cpus;
+    int max_cpus;
+    FWCfgState *fw_cfg;
+    AcpiMadtInfo *madt_info;
+    AcpiDsdtInfo *dsdt_info;
+    AcpiGtdtInfo *gtdt_info;
+} VirtGuestInfo;
+
+
+typedef struct VirtGuestInfoState {
+    VirtGuestInfo info;
+    Notifier machine_done;
+} VirtGuestInfoState;
+
+void virt_acpi_setup(VirtGuestInfo *guest_info);
+
+#endif
