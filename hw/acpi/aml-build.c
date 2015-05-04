e64d5ed1b97ce146ead4d88906d590c21d949653
Shannon Zhao <shannon.zhao@linaro.org>
2015-05-04 15:33:30 +0800

    hw/acpi/aml-build: Add Unicode macro
    Signed-off-by: Shannon Zhao <zhaoshenglong@huawei.com>
    Signed-off-by: Shannon Zhao <shannon.zhao@linaro.org>

--- hw/acpi/aml-build.c
+++ hw/acpi/aml-build.c
@@ -1061,6 +1061,24 @@ Aml *aml_touuid(const char *uuid)
     return var;
 }
 
+/*
+ * ACPI 2.0b: 16.2.3.6.4.3  Unicode Macro (Convert Ascii String To Unicode)
+ */
+Aml *aml_unicode(const char *str)
+{
+    Aml *var = aml_bundle(0x11 /* BufferOp */, AML_BUFFER);
+
+    while (*str != '\0') {
+        build_append_byte(var->buf, 0);
+        build_append_byte(var->buf, *str);
+        str++;
+    }
+    build_append_byte(var->buf, 0);
+    build_append_byte(var->buf, *str);
+
+    return var;
+}
+
 void
 build_header(GArray *linker, GArray *table_data,
              AcpiTableHeader *h, const char *sig, int len, uint8_t rev)
