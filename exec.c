156e0adf6be5c79363816887635bc7e3b72c5bbb
Alex Bennée <alex.bennee@linaro.org>
2015-12-14 14:04:32 +0000

    exec.c: add tb_lock() around breakpoint invalidation

--- exec.c
+++ exec.c
@@ -647,11 +647,13 @@ static void breakpoint_invalidate(CPUState *cpu, target_ulong pc)
 #else
 static void breakpoint_invalidate(CPUState *cpu, target_ulong pc)
 {
+    tb_lock();
     hwaddr phys = cpu_get_phys_page_debug(cpu, pc);
     if (phys != -1) {
         tb_invalidate_phys_addr(cpu->as,
                                 phys | (pc & ~TARGET_PAGE_MASK));
     }
+    tb_unlock();
 }
 #endif
 
