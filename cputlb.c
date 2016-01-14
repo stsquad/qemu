940238d6923dc7d0a72fe4dfb4d67fc9946ebca9
Alex Bennée <alex.bennee@linaro.org>
2016-01-14 14:10:05 +0000

    cputlb: add assertions for cpu == current_cpu
    The TLB structures are per-CPU although can be affected by other CPUs in
    the system. However any work that modifies the structures has to be in
    the current_cpu context to ensure running TCG code doesn't fall over in
    the process.
    
    A bunch of functions has been converted to punt stuff to async work but
    there are still more to do. In the meantime I've added assertions
    through the code so we get an assert rather than a random crash.
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    
    ---
    v8
      - re-based from previous version
      - fix up tlb flush/merge with earlier asserts for debugging

--- cputlb.c
+++ cputlb.c
@@ -66,6 +66,10 @@ static void tlb_flush_nocheck(CPUState *cpu, int flush_global)
 
     tlb_debug("(%d)\n", flush_global);
 
+    /* Either we are in the right cpu, or it is not running (which can
+       be the case when we are creating the machine */
+    g_assert((cpu == current_cpu) || !cpu->created);
+
     /* must reset current TB so that interrupts cannot modify the
        links while we are modifying them */
     cpu->current_tb = NULL;
@@ -130,7 +134,10 @@ static inline void v_tlb_flush_by_mmuidx(CPUState *cpu, va_list argp)
 {
     CPUArchState *env = cpu->env_ptr;
 
-    tlb_debug("start\n");
+    tlb_debug("(%p)\n", cpu);
+
+    g_assert(cpu == current_cpu);
+
     /* must reset current TB so that interrupts cannot modify the
        links while we are modifying them */
     cpu->current_tb = NULL;
@@ -154,6 +161,9 @@ static inline void v_tlb_flush_by_mmuidx(CPUState *cpu, va_list argp)
 void tlb_flush_by_mmuidx(CPUState *cpu, ...)
 {
     va_list argp;
+
+    g_assert(cpu == current_cpu);
+
     va_start(argp, cpu);
     v_tlb_flush_by_mmuidx(cpu, argp);
     va_end(argp);
@@ -180,6 +190,8 @@ void tlb_flush_page(CPUState *cpu, target_ulong addr)
     g_assert(current_cpu == cpu);
 
     tlb_debug("page :" TARGET_FMT_lx "\n", addr);
+
+    g_assert(cpu == current_cpu);
 
     /* Check if we need to flush due to large pages.  */
     if ((addr & env->tlb_flush_mask) == env->tlb_flush_addr) {
@@ -216,6 +228,8 @@ void tlb_flush_page_by_mmuidx(CPUState *cpu, target_ulong addr, ...)
     CPUArchState *env = cpu->env_ptr;
     int i, k;
     va_list argp;
+
+    g_assert(current_cpu == cpu);
 
     va_start(argp, addr);
 
@@ -375,6 +389,8 @@ void tlb_set_dirty(CPUState *cpu, target_ulong vaddr)
     int i;
     int mmu_idx;
 
+    g_assert(cpu == current_cpu);
+
     vaddr &= TARGET_PAGE_MASK;
     i = (vaddr >> TARGET_PAGE_BITS) & (CPU_TLB_SIZE - 1);
     for (mmu_idx = 0; mmu_idx < NB_MMU_MODES; mmu_idx++) {
@@ -434,7 +450,9 @@ void tlb_set_page_with_attrs(CPUState *cpu, target_ulong vaddr,
     unsigned vidx = env->vtlb_index++ % CPU_VTLB_SIZE;
     int asidx = cpu_asidx_from_attrs(cpu, attrs);
 
-    assert(size >= TARGET_PAGE_SIZE);
+    g_assert(cpu == current_cpu);
+    g_assert(size >= TARGET_PAGE_SIZE);
+
     if (size != TARGET_PAGE_SIZE) {
         tlb_add_large_page(env, vaddr, size);
     }
@@ -523,6 +541,8 @@ tb_page_addr_t get_page_addr_code(CPUArchState *env1, target_ulong addr)
     MemoryRegion *mr;
     CPUState *cpu = ENV_GET_CPU(env1);
     CPUIOTLBEntry *iotlbentry;
+
+    g_assert(cpu == current_cpu);
 
     page_index = (addr >> TARGET_PAGE_BITS) & (CPU_TLB_SIZE - 1);
     mmu_idx = cpu_mmu_index(env1, true);
