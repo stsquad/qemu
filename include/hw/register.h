0f878a424347cdd3ff13c8f6fef7f9996c4c490e
Peter Crosthwaite <peter.crosthwaite@xilinx.com>
2016-03-08 13:06:44 -0800

    register: QOMify
    QOMify registers as a child of TYPE_DEVICE. This allows registers to
    define GPIOs.
    
    Define an init helper that will do QOM initialisation.
    
    Signed-off-by: Peter Crosthwaite <peter.crosthwaite@xilinx.com>
    Signed-off-by: Alistair Francis <alistair.francis@xilinx.com>
    Reviewed-by: KONRAD Frederic <fred.konrad@greensocs.com>

--- include/hw/register.h
+++ include/hw/register.h
@@ -11,6 +11,7 @@
 #ifndef REGISTER_H
 #define REGISTER_H
 
+#include "hw/qdev-core.h"
 #include "exec/memory.h"
 
 typedef struct RegisterInfo RegisterInfo;
@@ -79,6 +80,9 @@ struct RegisterAccessInfo {
  */
 
 struct RegisterInfo {
+    /* <private> */
+    DeviceState parent_obj;
+
     /* <public> */
     void *data;
     int data_size;
@@ -90,6 +94,9 @@ struct RegisterInfo {
 
     void *opaque;
 };
+
+#define TYPE_REGISTER "qemu,register"
+#define REGISTER(obj) OBJECT_CHECK(RegisterInfo, (obj), TYPE_REGISTER)
 
 /**
  * This structure is used to group all of the individual registers which are
@@ -134,6 +141,14 @@ uint64_t register_read(RegisterInfo *reg);
  */
 
 void register_reset(RegisterInfo *reg);
+
+/**
+ * Initialize a register. GPIO's are setup as IOs to the specified device.
+ * Fast paths for eligible registers are enabled.
+ * @reg: Register to initialize
+ */
+
+void register_init(RegisterInfo *reg);
 
 /**
  * Memory API MMIO write handler that will write to a Register API register.
