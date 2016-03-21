dae163b44f25bc9bc67b694c6209c69cd54e0181
Alex Bennée <alex.bennee@linaro.org>
2016-03-21 19:54:57 +0000

    .travis.yml: enable OSX builds
    Travis has support for OSX builds. Making the setup work cleanly
    involves a little hacking about with the .travis.yml file but rather
    than make it too messy I've pushed all the "brew" install stuff into a
    support script called ./scripts/macosx-brew.sh.
    
    Currently only the default ./configure ${CONFIG} is built as I'm not
    sure what extra coverage would come from the other build stanzas.
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>

---
+++ scripts/macosx-brew.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Install MacOSX dependancies
+#
+brew update
+brew install libffi
+brew install gettext
+brew install pkg-config
+brew install glib
+brew install autoconf
+brew install automake
+brew install pixman
