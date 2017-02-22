f6e123463d71cd1e78dfc03f6a95528b1cb0ba5e
Alex Bennée <alex.bennee@linaro.org>
2017-02-22 16:25:06 +0000

    hmp: add PC information for ARM vCPUs
    This continues an existing #ifdef ladder.
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>

--- cpus.c
+++ cpus.c
@@ -1897,6 +1897,9 @@ CpuInfoList *qmp_query_cpus(Error **errp)
 #elif defined(TARGET_TRICORE)
         TriCoreCPU *tricore_cpu = TRICORE_CPU(cpu);
         CPUTriCoreState *env = &tricore_cpu->env;
+#elif defined(TARGET_AARCH64) || defined (TARGET_ARM)
+        ARMCPU *arm_cpu = ARM_CPU(cpu);
+        CPUARMState *env = &arm_cpu->env;
 #endif
 
         cpu_synchronize_state(cpu);
@@ -1924,6 +1927,9 @@ CpuInfoList *qmp_query_cpus(Error **errp)
 #elif defined(TARGET_TRICORE)
         info->value->arch = CPU_INFO_ARCH_TRICORE;
         info->value->u.tricore.PC = env->PC;
+#elif defined(TARGET_AARCH64) || defined(TARGET_ARM)
+        info->value->arch = CPU_INFO_ARCH_ARM;
+        info->value->u.arm.pc = env->pc;
 #else
         info->value->arch = CPU_INFO_ARCH_OTHER;
 #endif
