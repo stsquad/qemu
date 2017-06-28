a53a445f79353f4792782462a48893f68da53f84
Alex Bennée <alex.bennee@linaro.org>
2017-06-28 18:06:21 +0100

    arm/translate-a64: add FP16 FADD/FSUB/FMUL/FDIV to simd_three_reg_same_fp16
    The fprintf is only there for debugging as the skeleton is added to,
    it will be removed once the skeleton is complete.
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>

--- target/arm/translate-a64.c
+++ target/arm/translate-a64.c
@@ -9828,6 +9828,18 @@ static void disas_simd_three_reg_same_fp16(DisasContext *s, uint32_t insn)
         read_vec_element_i32(s, tcg_op2, rm, pass, MO_16);
 
         switch (fpopcode) {
+        case 0x2: /* FADD */
+            gen_helper_advsimd_addh(tcg_res, tcg_op1, tcg_op2, fpst);
+            break;
+        case 0xa: /* FSUB */
+            gen_helper_advsimd_subh(tcg_res, tcg_op1, tcg_op2, fpst);
+            break;
+        case 0x23: /* FMUL */
+            gen_helper_advsimd_mulh(tcg_res, tcg_op1, tcg_op2, fpst);
+            break;
+        case 0x27: /* FDIV */
+            gen_helper_advsimd_divh(tcg_res, tcg_op1, tcg_op2, fpst);
+            break;
         default:
             fprintf(stderr, "%s: insn %#04x, fpop %#2x @ %#" PRIx64 "\n",
                     __func__, insn, fpopcode, s->pc);
