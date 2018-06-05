53738e0ae5e547cae4d8b705a6978136fe25862b
Pavel Dovgalyuk <Pavel.Dovgaluk@ispras.ru>
2018-06-05 13:39:26 +0300

    Add plugin support
    This patch adds support for dynamically loaded plugins.
    Every plugin is a dynamic library with a set of optional exported
    functions that will be called from QEMU.
    
    Signed-off-by: Pavel Dovgalyuk <Pavel.Dovgaluk@ispras.ru>

--- qemu-options.hx
+++ qemu-options.hx
@@ -3920,6 +3920,16 @@ STEXI
 Enable synchronization profiling.
 ETEXI
 
+#ifdef CONFIG_PLUGINS
+DEF("plugin", HAS_ARG, QEMU_OPTION_plugin, \
+           "-plugin file=<file>[,args=<args>] load <dso> plugin with <args>\n", QEMU_ARCH_ALL)
+STEXI
+@item -plugin file=@var{file}[,args=@var{args}]
+@findex -plugin
+Load @var{file} plugin passing @var{args} arguments.
+ETEXI
+#endif
+
 STEXI
 @end table
 ETEXI
