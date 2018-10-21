7e23ea41d0f1b07480c5508949f8af4ed19ffa34
Emilio G. Cota <cota@braap.org>
2018-10-21 13:27:44 -0400

    *-user: plugin syscalls
    Signed-off-by: Emilio G. Cota <cota@braap.org>

--- linux-user/syscall.c
+++ linux-user/syscall.c
@@ -11504,6 +11504,8 @@ abi_long do_syscall(void *cpu_env, int num, abi_long arg1,
 
     trace_guest_user_syscall(cpu, num, arg1, arg2, arg3, arg4,
                              arg5, arg6, arg7, arg8);
+    qemu_plugin_vcpu_syscall(cpu, num, arg1, arg2, arg3, arg4, arg5, arg6, arg7,
+                             arg8);
 
     if (unlikely(do_strace)) {
         print_syscall(num, arg1, arg2, arg3, arg4, arg5, arg6);
@@ -11516,5 +11518,6 @@ abi_long do_syscall(void *cpu_env, int num, abi_long arg1,
     }
 
     trace_guest_user_syscall_ret(cpu, num, ret);
+    qemu_plugin_vcpu_syscall_ret(cpu, num, ret);
     return ret;
 }
