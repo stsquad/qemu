ad54559314ca144e146e82b0b3034fe8761081c6
Emilio G. Cota <cota@braap.org>
2018-12-14 13:59:11 -0500

    Revert "translate-all: make l1_map lockless"
    This reverts commit 78722ed0b826644ae240e3c0bbb6bdde02dfe7e1.

--- accel/tcg/translate-all.c
+++ accel/tcg/translate-all.c
@@ -469,11 +469,19 @@ static void page_init(void)
 #endif
 }
 
+/* If alloc=1:
+ * Called with tb_lock held for system emulation.
+ * Called with mmap_lock held for user-mode emulation.
+ */
 static PageDesc *page_find_alloc(tb_page_addr_t index, int alloc)
 {
     PageDesc *pd;
     void **lp;
     int i;
+
+    if (alloc) {
+        assert_memory_lock();
+    }
 
     /* Level 1.  Always allocated.  */
     lp = l1_map + ((index >> v_l1_shift) & (v_l1_size - 1));
@@ -483,17 +491,11 @@ static PageDesc *page_find_alloc(tb_page_addr_t index, int alloc)
         void **p = atomic_rcu_read(lp);
 
         if (p == NULL) {
-            void *existing;
-
             if (!alloc) {
                 return NULL;
             }
             p = g_new0(void *, V_L2_SIZE);
-            existing = atomic_cmpxchg(lp, NULL, p);
-            if (unlikely(existing)) {
-                g_free(p);
-                p = existing;
-            }
+            atomic_rcu_set(lp, p);
         }
 
         lp = p + ((index >> (i * V_L2_BITS)) & (V_L2_SIZE - 1));
@@ -501,17 +503,11 @@ static PageDesc *page_find_alloc(tb_page_addr_t index, int alloc)
 
     pd = atomic_rcu_read(lp);
     if (pd == NULL) {
-        void *existing;
-
         if (!alloc) {
             return NULL;
         }
         pd = g_new0(PageDesc, V_L2_SIZE);
-        existing = atomic_cmpxchg(lp, NULL, pd);
-        if (unlikely(existing)) {
-            g_free(pd);
-            pd = existing;
-        }
+        atomic_rcu_set(lp, pd);
     }
 
     return pd + (index & (V_L2_SIZE - 1));
