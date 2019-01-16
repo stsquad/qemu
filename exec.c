e181655e66433b80ced9302f768f55802bfe3a75
Alex Bennée <alex.bennee@linaro.org>
2019-01-16 16:18:12 +0000

    exec.c: remove #include "tcg.h"
    We don't really need the in-depth details of the TCG exposed to this
    file. The only thing we needed is common code now.
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>

--- exec.c
+++ exec.c
@@ -23,7 +23,6 @@
 #include "cpu.h"
 #include "exec/exec-all.h"
 #include "exec/target_page.h"
-#include "tcg.h"
 #include "hw/qdev-core.h"
 #include "hw/qdev-properties.h"
 #if !defined(CONFIG_USER_ONLY)
