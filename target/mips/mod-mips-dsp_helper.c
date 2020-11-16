1350585415b703ae5973588ff45b5e0b5afcd7cf
Philippe Mathieu-Daudé <f4bug@amsat.org>
2020-11-16 05:33:32 +0100

    target/mips: Extract DSP translation routines
    Extract 2150 lines from the huge translate.c to a new file,
    'mod-mips-dsp_translate.c.inc'. As there are too many inter-
    dependencies we don't compile it as another object, but
    keep including it in the big translate.o. We gain in code
    maintainability.
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>

--- target/mips/mod-mips-dsp_helper.c
+++ target/mips/mod-mips-dsp_helper.c
@@ -15,6 +15,8 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
  */
 
 #include "qemu/osdep.h"
