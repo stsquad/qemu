c6fe7648b0bf1f4edc244a01f4cf6492929fd3cf
Philippe Mathieu-Daudé <f4bug@amsat.org>
2020-11-30 14:10:32 +0100

    target/mips: Use decode_msa32() generated from decodetree
    Now that we can decode the MSA ASE opcodes with decode_msa32(),
    use it and remove the unreachable code.
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>

--- target/mips/translate.h
+++ target/mips/translate.h
@@ -82,8 +82,6 @@ extern TCGv bcond;
 
 /* MSA */
 void msa_translate_init(void);
-void gen_msa(DisasContext *ctx);
-void gen_msa_branch(DisasContext *ctx, uint32_t op1);
 bool decode_msa32(DisasContext *ctx, uint32_t insn);
 
 #endif
