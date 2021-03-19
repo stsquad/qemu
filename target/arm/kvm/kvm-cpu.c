1d4d4bedea4f3d8173856e833ecee0f2285421f6
Claudio Fontana <cfontana@suse.de>
2021-03-19 11:55:19 +0100

    XXX target/arm: experiment refactoring cpu "max"
    XXX Someone who really understands which properties should be added
    where should review this attentively.
    
    What goes into cpu leaf class initialization?
    What goes into arm_post_init / accel_cpu?
    What goes into arm_cpu_finalize_features / aarch64_cpu_finalize_features?
    
    Should there be shift of more code into finalize_features?
    
    Signed-off-by: Claudio Fontana <cfontana@suse.de>
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>

--- target/arm/kvm/kvm-cpu.c
+++ target/arm/kvm/kvm-cpu.c
@@ -88,9 +88,7 @@ static void host_cpu_instance_init(Object *obj)
     ARMCPU *cpu = ARM_CPU(obj);
 
     kvm_arm_set_cpu_features_from_host(cpu);
-    if (arm_feature(&cpu->env, ARM_FEATURE_AARCH64)) {
-        cpu_sve_add_props(obj);
-    }
+    cpu_sve_add_props(obj);
     arm_cpu_post_init(obj);
 }
 
