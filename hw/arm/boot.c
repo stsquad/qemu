98c12848756788659e7ce6214329da00b1ca23a5
Claudio Fontana <cfontana@suse.de>
2021-03-22 15:01:32 +0100

    target/arm: add temporary stub for arm_rebuild_hflags
    this should go away once the configuration and hw/arm is clean
    
    Signed-off-by: Claudio Fontana <cfontana@suse.de>
    Message-Id: <20210322140206.9513-16-cfontana@suse.de>

--- hw/arm/boot.c
+++ hw/arm/boot.c
@@ -27,6 +27,7 @@
 #include "qemu/option.h"
 #include "exec/address-spaces.h"
 #include "qemu/units.h"
+#include "sysemu/tcg.h"
 
 /* Kernel boot protocol is specified in the kernel docs
  * Documentation/arm/Booting and Documentation/arm64/booting.txt
@@ -797,7 +798,9 @@ static void do_cpu_reset(void *opaque)
                 info->secondary_cpu_reset_hook(cpu, info);
             }
         }
-        arm_rebuild_hflags(env);
+        if (tcg_enabled()) {
+            arm_rebuild_hflags(env);
+        }
     }
 }
 
