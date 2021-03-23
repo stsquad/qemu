9791d5d0cb6b2177d943b107152b77452c20bd34
Claudio Fontana <cfontana@suse.de>
2021-03-23 16:46:01 +0100

    target/arm: kvm: add stubs for some helpers
    at least the armv7m one should go away with proper
    configuration changes (only enabling possible boards for KVM).
    
    Signed-off-by: Claudio Fontana <cfontana@suse.de>
    Message-Id: <20210323154639.23477-10-cfontana@suse.de>

---
+++ target/arm/kvm/helper-stubs.c
@@ -0,0 +1,27 @@
+/*
+ * QEMU ARM KVM stubs for some helper functions
+ *
+ * Copyright 2021 SUSE LLC
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ */
+
+#include "qemu/osdep.h"
+#include "cpu.h"
+
+/* return the effective value of HCR_EL2. For KVM, always 0. */
+uint64_t arm_hcr_el2_eff(CPUARMState *env)
+{
+    return 0;
+}
+
+int fp_exception_el(CPUARMState *env, int cur_el)
+{
+    return 0;
+}
+
+void write_v7m_exception(CPUARMState *env, uint32_t new_exc)
+{
+    g_assert_not_reached();
+}
