4b3445813c261dbc5d1591618efba2cebc0e9504
Dmitry Osipenko <dmitry.osipenko@collabora.com>
2024-05-20 00:27:10 +0300

    virtio-gpu: Move print_stats timer to VirtIOGPUGL
    Move print_stats timer to VirtIOGPUGL for consistency with
    cmdq_resume_bh and fence_poll that are used only by GL device.
    
    Signed-off-by: Dmitry Osipenko <dmitry.osipenko@collabora.com>
    Message-Id: <20240519212712.2605419-12-dmitry.osipenko@collabora.com>
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>

--- include/hw/virtio/virtio-gpu.h
+++ include/hw/virtio/virtio-gpu.h
@@ -195,7 +195,6 @@ struct VirtIOGPU {
     uint64_t hostmem;
 
     bool processing_cmdq;
-    QEMUTimer *print_stats;
 
     uint32_t inflight;
     struct {
@@ -233,6 +232,7 @@ struct VirtIOGPUGL {
 
     QEMUBH *cmdq_resume_bh;
     QEMUTimer *fence_poll;
+    QEMUTimer *print_stats;
 };
 
 struct VhostUserGPU {
