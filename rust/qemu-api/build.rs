65781a46693cfdc6e399a853248035c0897dd03d
Paolo Bonzini <pbonzini@redhat.com>
2024-11-08 19:01:29 +0100

    rust: qemu_api: do not disable lints outside bindgen-generated code
    rust/qemu-api/src/lib.rs is disabling lints that cause problems
    with code generated by bindgen.  Instead, include the bindgen
    code via include!(...) and move the #![allow()] directives
    into the bindings module.
    
    Add MESON_BUILD_ROOT to the devenv, so that it's easy for
    build.rs to find the include file.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Message-Id: <20241108180139.117112-2-pbonzini@redhat.com>
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>

--- rust/qemu-api/build.rs
+++ rust/qemu-api/build.rs
@@ -2,17 +2,25 @@
 // Author(s): Manos Pitsidianakis <manos.pitsidianakis@linaro.org>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-use std::path::Path;
+use std::{env, path::Path};
 
 use version_check as rustc;
 
 fn main() {
-    if !Path::new("src/bindings.rs").exists() {
-        panic!(
-            "No generated C bindings found! Either build them manually with bindgen or with meson \
-             (`ninja bindings.rs`) and copy them to src/bindings.rs, or build through meson."
-        );
+    // Placing bindings.rs.inc in the source directory is supported
+    // but not documented or encouraged.
+    let path = env::var("MESON_BUILD_ROOT")
+        .unwrap_or_else(|_| format!("{}/src", env!("CARGO_MANIFEST_DIR")));
+
+    let file = format!("{}/bindings.rs.inc", path);
+    if !Path::new(&file).exists() {
+        panic!(concat!(
+            "No generated C bindings found! If you want to run `cargo`, start a subshell\n",
+            "with `meson devenv`, or point MESON_BUILD_ROOT to the top of the build tree."
+        ));
     }
+
+    println!("cargo:rustc-env=BINDINGS_RS_INC={}", file);
 
     // Check for available rustc features
     if rustc::is_min_version("1.77.0").unwrap_or(false) {
