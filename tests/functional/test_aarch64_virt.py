a2c8eb71d8d1ab3957dab436fec808fb3dbfecc5
Alex Bennée <alex.bennee@linaro.org>
2024-12-10 15:56:13 +0000

    tests/functional: extend test_aarch64_virt with vulkan test
    Now we have virtio-gpu Vulkan support lets add a test for it.
    Currently this is using images build by buildroot:
    
      https://lists.buildroot.org/pipermail/buildroot/2024-December/768196.html
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    
    ---
    v2
      - use decorator for has_cmd(zstd)
      - move set_machine/require_accelerator to top of test
      - un-handled->unhandled
      - drop extra - from --snapshot
      - drop unneeded virtualization=on

--- tests/functional/test_aarch64_virt.py
+++ tests/functional/test_aarch64_virt.py
@@ -14,11 +14,13 @@ import time
 import os
 import logging
 
+from qemu.machine.machine import VMLaunchFailure
+
 from qemu_test import BUILD_DIR
 from qemu_test import QemuSystemTest, Asset
-from qemu_test import exec_command, wait_for_console_pattern
-from qemu_test import get_qemu_img, run_cmd
-
+from qemu_test import exec_command, wait_for_console_pattern, exec_command_and_wait_for_pattern
+from qemu_test import has_cmd, get_qemu_img, run_cmd
+from unittest import skipUnless
 
 class Aarch64VirtMachine(QemuSystemTest):
     KERNEL_COMMON_COMMAND_LINE = 'printk.time=0 '
@@ -123,6 +125,77 @@ class Aarch64VirtMachine(QemuSystemTest):
 
     def test_aarch64_virt_gicv2(self):
         self.common_aarch64_virt("virt,gic-version=2")
+
+
+    ASSET_VIRT_GPU_KERNEL = Asset(
+        ('https://fileserver.linaro.org/s/ce5jXBFinPxtEdx/'
+         'download?path=%2F&files='
+         'Image'),
+        '89e5099d26166204cc5ca4bb6d1a11b92c217e1f82ec67e3ba363d09157462f6')
+
+    ASSET_VIRT_GPU_ROOTFS = Asset(
+        ('https://fileserver.linaro.org/s/ce5jXBFinPxtEdx/'
+         'download?path=%2F&files='
+         'rootfs.ext4.zstd'),
+        '792da7573f5dc2913ddb7c638151d4a6b2d028a4cb2afb38add513c1924bdad4')
+
+    @skipUnless(*has_cmd('zstd'))
+    def test_aarch64_virt_with_gpu(self):
+        # This tests boots with a buildroot test image that contains
+        # vkmark and other GPU exercising tools. We run a headless
+        # weston that nevertheless still exercises the virtio-gpu
+        # backend.
+
+        self.set_machine('virt')
+        self.require_accelerator("tcg")
+
+        image_path_zst = self.ASSET_VIRT_GPU_ROOTFS.fetch()
+        kernel_path = self.ASSET_VIRT_GPU_KERNEL.fetch()
+
+        image_path = self.workdir + "/rootfs.ext4"
+
+        run_cmd(['zstd', "-f", "-d", image_path_zst,
+                 "-o", image_path])
+
+        self.vm.set_console()
+        kernel_command_line = (self.KERNEL_COMMON_COMMAND_LINE +
+                               'console=ttyAMA0 root=/dev/vda')
+
+        self.vm.add_args("-accel", "tcg")
+        self.vm.add_args("-cpu", "neoverse-v1,pauth-impdef=on")
+        self.vm.add_args("-machine", "virt,gic-version=max",
+                         '-kernel', kernel_path,
+                         '-append', kernel_command_line)
+        self.vm.add_args("-smp", "2", "-m", "2048")
+        self.vm.add_args("-device", "virtio-gpu-gl-pci,hostmem=4G,blob=on,venus=on")
+        self.vm.add_args("-display", "egl-headless")
+        self.vm.add_args("-display", "dbus,gl=on")
+        self.vm.add_args("-device", "virtio-blk-device,drive=hd0")
+        self.vm.add_args("-blockdev",
+                         "driver=raw,file.driver=file,node-name=hd0,read-only=on,"
+                         f"file.filename={image_path}")
+        self.vm.add_args("-snapshot")
+
+        try:
+            self.vm.launch()
+        except VMLaunchFailure as e:
+            if "old virglrenderer, blob resources unsupported" in e.output:
+                self.skipTest("No blob support for virtio-gpu")
+            elif "old virglrenderer, venus unsupported" in e.output:
+                self.skipTest("No venus support for virtio-gpu")
+            else:
+                self.log.info(f"unhandled launch failure: {e.output}")
+                raise e
+
+        self.wait_for_console_pattern('buildroot login:')
+        exec_command(self, 'root')
+        exec_command(self, 'export XDG_RUNTIME_DIR=/tmp')
+        exec_command_and_wait_for_pattern(self,
+                                          "weston -B headless "
+                                          "--renderer gl "
+                                          "--shell kiosk "
+                                          "-- vkmark",
+                                          "vkmark Score")
 
 
 if __name__ == '__main__':
