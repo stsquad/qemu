fa7ee8c64af8c015b3e161f032876f8c7626abf7
Alex Bennée <alex.bennee@linaro.org>
2025-06-12 13:44:51 +0100

    kvm/arm: allow out-of kernel GICv3 to work with KVM
    Previously we suppressed this option as KVM would get confused if it
    started trapping GIC system registers without a GIC configured.
    However if we know we are trapping harder we can allow it much like we
    do for HVF.
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>

--- target/arm/kvm_arm.h
+++ target/arm/kvm_arm.h
@@ -243,6 +243,14 @@ static inline bool kvm_arm_el2_supported(void)
 int kvm_arm_get_type(MachineState *ms);
 
 /**
+ * kvm_arm_is_trapping_harder: return true if trapping harder
+ * @ms: Machine state handle
+ *
+ * return true if trapping harder
+ */
+bool kvm_arm_is_trapping_harder(MachineState *ms);
+
+/**
  * kvm_arm_get_max_vm_ipa_size:
  * @ms: Machine state handle
  * @fixed_ipa: True when the IPA limit is fixed at 40. This is the case
